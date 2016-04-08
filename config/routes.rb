a07851e52ed48c797d3f64b6303b002abe3e5e27
donglujun <donglujun@bayekeji.com>
2016-04-08 14:27:01 +0800

    home page change

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
+  root 'messages#index'
   resources :messages
   get 'static_pages/home'
 
   get 'static_pages/help'
 
   get 'static_pages/about'
-
-  resources :messages
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
