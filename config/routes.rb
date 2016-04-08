7a9053cd46b74da8ed066106c7e48f92de07b521
donglujun <donglujun@bayekeji.com>
2016-04-08 11:58:01 +0800

    增加message数据模型

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  resources :messages
   get 'static_pages/home'
 
   get 'static_pages/help'
 
   get 'static_pages/about'
+
+  resources :messages
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
