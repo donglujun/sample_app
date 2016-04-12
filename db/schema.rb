b97fb2b1cd6cf5c2cefdebea5b545d4ba201bbb5
donglujun <donglujun@bayekeji.com>
2016-04-12 15:28:41 +0800

    使用simditor编辑器

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408033337) do
+ActiveRecord::Schema.define(version: 20160408032036) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "title"
