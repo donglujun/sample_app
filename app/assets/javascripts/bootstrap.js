bf22f0434b4f1e78b4930f58f58ccca228949f6c
donglujun <ghoststar001@gmail.com>
2016-06-27 14:37:20 +0800

    滚动条

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -1,4 +1,7 @@
 jQuery(function() {
   $("a[rel~=popover], .has-popover").popover();
   $("a[rel~=tooltip], .has-tooltip").tooltip();
+  if ($(".main").length == 0) {
+    $("body").css("overflow", "auto");
+  }
 });
